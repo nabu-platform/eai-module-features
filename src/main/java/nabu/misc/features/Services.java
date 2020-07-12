8f609bf4ac13845a1e3db404983a8230aaa2793f
Alexander Verbruggen <alex@nabu.be>
2020-07-12 21:25:53 +0200

    updated

--- src/main/java/nabu/misc/features/Services.java
+++ src/main/java/nabu/misc/features/Services.java
@@ -17,15 +17,21 @@ import be.nabu.libs.artifacts.api.Artifact;
 @WebService
 public class Services {
 	
-	public void update(@NotNull @WebParam(name = "featureSetId") String featureSetId, @WebParam(name = "enabledFeatures") List<String> enabledFeatures) throws IOException {
+	public void update(@NotNull @WebParam(name = "featureSetId") String featureSetId, @WebParam(name = "enabledFeatures") List<String> enabledFeatures, @WebParam(name = "persist") Boolean persist) throws IOException {
 		Artifact resolve = EAIResourceRepository.getInstance().resolve(featureSetId);
 		if (resolve instanceof FeatureSet) {
-			((FeatureSet) resolve).getConfig().setFeatures(enabledFeatures);
-			new FeatureSetManager().save((ResourceEntry) EAIResourceRepository.getInstance().getEntry(resolve.getId()), (FeatureSet) resolve);
+			if (((FeatureSet) resolve).getConfig().getFeatures() == null) {
+				((FeatureSet) resolve).getConfig().setFeatures(new ArrayList<String>());
+			}
+			((FeatureSet) resolve).getConfig().getFeatures().clear();
+			((FeatureSet) resolve).getConfig().getFeatures().addAll(enabledFeatures);
+			if (persist != null && persist) {
+				new FeatureSetManager().save((ResourceEntry) EAIResourceRepository.getInstance().getEntry(resolve.getId()), (FeatureSet) resolve);
+			}
 		}
 	}
 	
-	public void toggle(@NotNull @WebParam(name = "featureSetId") String featureSetId, @WebParam(name = "feature") String feature, @WebParam(name = "enabled") Boolean enabled) throws IOException {
+	public void toggle(@NotNull @WebParam(name = "featureSetId") String featureSetId, @WebParam(name = "feature") String feature, @WebParam(name = "enabled") Boolean enabled, @WebParam(name = "persist") Boolean persist) throws IOException {
 		if (feature != null) {
 			Artifact resolve = EAIResourceRepository.getInstance().resolve(featureSetId);
 			if (resolve instanceof FeatureSet) {
@@ -40,7 +46,9 @@ public class Services {
 				else if (!features.contains(feature)) {
 					features.add(feature);
 				}
-				new FeatureSetManager().save((ResourceEntry) EAIResourceRepository.getInstance().getEntry(resolve.getId()), (FeatureSet) resolve);
+				if (persist != null && persist) {
+					new FeatureSetManager().save((ResourceEntry) EAIResourceRepository.getInstance().getEntry(resolve.getId()), (FeatureSet) resolve);
+				}
 			}
 		}
 	}
